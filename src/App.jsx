fbeb6f6e960f197b3385ac10aab2b981a2616ea0
Aman Patel <aman17626@gmail.com>
2023-11-25 21:07:16 +0530

    minor bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useReducer } from "react";
 import DigitButton from "./components/DigitButton";
 import OperationButton from "./components/OperationButton";
-import "./index.css";
+import "../src/index.css";
 
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
